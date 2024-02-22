becc9b1f98a2c1409dc3a9504a2c15d2c4a64fa0
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-22 18:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function stringChop(str, size) {
   // your code here
+	const result = [];
+	size = size - 0;
+
+	let start = 0;
+
+	while(start < str.length){
+		console.log(start+" "+(start+size));
+		if(start+size < str.length){
+			result.push(str.slice(start,start+size));
+		}else{
+			result.push(str.slice(start, str.length));
+		}
+		start += (size);
+	}
+	return result;
+
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
